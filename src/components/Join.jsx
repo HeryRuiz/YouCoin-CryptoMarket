f7852e8a045149901cc2c98785befc01b3f80f62
Hery Ruiz <heryrdev@gmail.com>
2023-12-12 18:12:29 -0500

    added back the join coins:

--- src/components/Join.jsx
+++ src/components/Join.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import "../styles/Join.css";
-
+import Btc from "../images/herojoin/bitcoin.png";
+import Eth from "../images/herojoin/ethereum.png";
 
 function Join() {
   return (
     <>
       <section id="join" className="join-section">
         <div className="join-content">
+          <img src={Btc} alt="Btc Coin" className="floating-coin" />
           <div className="join-content__text">
             <h2>
               JOIN US VIA <br /> <span>DISCORD</span>
@@ -16,6 +18,7 @@ function Join() {
               Join via Discord
             </a>
           </div>
+          <img src={Eth} alt="Eth Coin" className="floating-coin" />
         </div>
       </section>
     </>
