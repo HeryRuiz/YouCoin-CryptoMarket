a96b22dbe9203e84310879a54bb93aab33e88e3c
Hery Ruiz <heryrdev@gmail.com>
2024-01-02 13:14:00 -0500

    formatted join.jsx

--- src/components/Home/Join.jsx
+++ src/components/Home/Join.jsx
@@ -6,9 +6,10 @@ function Join({ setArrowStyle, resetArrowStyle, toSection }) {
     <>
       <section id="join" className="join-section">
         <div className="join__content">
-
           <div className="join__content__text">
-            <h1>The future of crypto <br /> <span>starts here.</span></h1>
+            <h1>
+              The future of crypto <br /> <span>starts here.</span>
+            </h1>
             <div className="join__arrow__buttons">
               <button
                 onClick={() => toSection("market-section")}
